9c11b91b31984e157759b921797d68c2021e75ab
romyedith <romyedith@gmail.com>
2023-10-11 17:22:17 -0700

    Add files via upload

---
+++ screens/EEILocationScreen.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class EEILocationScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Localizacion EEI Dani</Text>
+      </View>
+    )
+  }
+}
+
+export default EEILocationScreen
+
---
+++ screens/MeteorScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class MeteorScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>PantallaMeteoritos</Text>
+      </View>
+    )
+  }
+}
+
+export default MeteorScreen
